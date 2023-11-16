72752cc53169540c8633f4c8f64326e9a8efc4f3
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2023-11-16 00:22:11 +0000

    inicio da funcao de stream

---
+++ src/protocol/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net"
+	"os"
+)
+
+const BUFSIZE = 1024 * 8
+
+func streamRequest(conn *net.UDPConn, client *net.UDPAddr, filePath string) {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		// ERROOOO
+		return
+	}
+
+	defer file.Close()
+
+	fi, err := file.Stat()
+
+	if err != nil {
+		// ERROOOO
+		return
+	}
+
+	//fileSize := int(fi.Size())
+
+	buffer := make([]byte, BUFSIZE)
+
+	for {
+		n, err := file.Read(buffer)
+
+		if n == 0 {
+			break
+		}
+
+		if err != nil {
+			break
+		}
+
+		data := buffer[:n]
+		conn.WriteToUDP(data, client)
+	}
+}
