6e68d7e71211e46d3a708d6236ceed3763545436
Gustavo <carlosgsp39@gmail.com>
2023-11-16 14:26:13 +0000

    unicast file transfer

--- src/client/main.go
+++ src/client/main.go
@@ -11,36 +11,90 @@ func main() {
 
 	// abrir a socket para receber pedidos
 	adr := "localhost:8080"
-	udpAddr, _ := net.ResolveUDPAddr("udp", adr)
-	listener, erro := net.DialUDP("udp", nil, udpAddr)
+	listener, erro := net.Dial("tcp", adr)
 	if erro != nil {
 		fmt.Println("Error:", erro)
 		return
 	}
 	defer listener.Close()
 
-	fmt.Printf("Client is connected at %s\n", adr)
+	fmt.Println("Client is connected at ", adr)
 
 	// manda cenas ao server
-	mandaCenas(listener, filePath)
-	fmt.Printf("Sent request for %s\n", filePath)
+	requestFile(listener, filePath)
 
-	// recebe cenas do server
-	recebeCenas(listener, filePath)
+	// Read server's response.
+	response := make([]byte, 4096)
+	n, err := listener.Read(response)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		os.Exit(1)
+	}
 
-	fmt.Printf("Closing connection...\n")
+	r := string(response[:n])
+
+	if r == "GO" {
+		fmt.Println("Server ready for file transfer")
+		fileTransfer(listener, filePath)
+		fmt.Println("Received file successfully")
+	} else {
+		fmt.Println("The file was not found")
+	}
+
+	fmt.Println("Closing connection...")
+	defer listener.Close()
 }
 
-func mandaCenas(conn *net.UDPConn, text string) {
-	_, err := conn.Write([]byte(text))
+func requestFile(conn net.Conn, filePath string) {
+	data := []byte(filePath)
+	_, err := conn.Write(data)
 	if err != nil {
-		fmt.Println("Error sending message:", err)
+		fmt.Println("Error:", err)
 		return
 	}
 }
 
-func recebeCenas(conn *net.UDPConn, filePath string) {
+func fileTransfer(connTCP net.Conn, filePath string) {
+	var packetCount int
+	packetChan := make(chan struct{})
+	// abre conn UDP
 
+	addr, err := net.ResolveUDPAddr("udp", connTCP.RemoteAddr().String())
+	addr.Port = 8081
+	listenerUDP, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer listenerUDP.Close()
+	fmt.Println("Connected at ", addr)
+	// recebe pacotes
+	go receivePackets(listenerUDP, filePath, packetChan, &packetCount)
+
+	//espera EOF
+	response := make([]byte, 4096)
+	msg, err := connTCP.Read(response)
+	r := string(response[:msg])
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		os.Exit(1)
+	}
+
+	//devolve numero de pacotes recebidos
+	if r == "EOF" {
+		close(packetChan)
+		fmt.Println(packetCount)
+		data := []byte(fmt.Sprintf("%d", packetCount))
+		_, err := connTCP.Write(data)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	}
+
+}
+
+func receivePackets(listener *net.UDPConn, filePath string, packetChan chan struct{}, packetCount *int) {
 	file, err := os.Create(filePath)
 	if err != nil {
 		fmt.Println("Error creating file:", err)
@@ -48,29 +102,24 @@ func recebeCenas(conn *net.UDPConn, filePath string) {
 	}
 	defer file.Close()
 
+	fmt.Println("Ready for file transfer")
+
 	// Set buffer size
-	bufferSize := 1024
-	buffer := make([]byte, bufferSize)
+	buffer := make([]byte, 1024)
 
 	// Loop to receive and write packets to the file
 	for {
-		// Receive a packet from the sender
-		n, _, err := conn.ReadFromUDP(buffer)
-		if err != nil {
-			fmt.Println("Error receiving data:", err)
+		select {
+		case <-packetChan:
+			fmt.Println("Stopping goroutine...")
 			return
-		}
-
-		var buf [1024]byte
-		data := string(buf[:n])
-		if data == "EOF" {
-			fmt.Println("File received successfully.")
-			_, err = conn.Write([]byte("EOF"))
+		default:
+			// Receive a packet from the sender
+			n, err := listener.Read(buffer)
 			if err != nil {
-				fmt.Println("Error sending message:", err)
+				fmt.Println("Error receiving data:", err)
+				return
 			}
-			return
-		} else {
 
 			// Write the received data to the file
 			_, err = file.Write(buffer[:n])
@@ -78,12 +127,8 @@ func recebeCenas(conn *net.UDPConn, filePath string) {
 				fmt.Println("Error writing to file:", err)
 				return
 			}
-
-			_, err = conn.Write([]byte("ACK"))
-			if err != nil {
-				fmt.Println("Error sending message:", err)
-				return
-			}
+			*packetCount++
 		}
+
 	}
 }
